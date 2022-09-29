1d120c88f5e65e7be0534c1b80c366835d66044d
Moxie03 <oluwatobilobatolulope@gmail.com>
2022-09-29 21:13:24 +0100

    Completed explore project

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+// alert()
+var burger = document.querySelector(".hamburger-container")
+var boxlink = document.querySelector(".mobile-link-box")
+
+function replace(){
+    burger.classList.toggle('displayburger')
+    boxlink.classList.toggle('displaylinkbox')
+}
